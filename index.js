1ed57206cd35e557df5f3feced949b52f4bb0038
mary mol <geethuakkuru@gmail.com>
2024-12-04 14:44:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//getElementById
+
+const myTitle = document.getElementById('main-title');
+ console.log(myTitle);
+
+ //getElementByClassName
+
+ const myContainer=document.getElementsByClassName('container');
+  console.log(myContainer);
+
+//class section className
+
+  const myClass=document.getElementsByClassName('section');
+  console.log(myClass);
+
+  //getElementByTagName
+
+  const paraNames=document.getElementsByTagName('p');
+   console.log( paraNames);
+
+   //querySelector for first button
+
+   const firstButton=document.querySelector('.btn');
+   console.log(firstButton);
+
+   //querySelectorAll for all buttons
+
+   const allButtons=document.querySelectorAll('.btn');
+   console.log(allButtons);
+
+   //select all p elements within the container
+
+   const pContainer=document.querySelectorAll('.container p');
+    console.log(pContainer);
